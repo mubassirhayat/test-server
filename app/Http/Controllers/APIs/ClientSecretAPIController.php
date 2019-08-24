1f07a13374b826ec26bd9ef3cae654095147f1ca
Mubassir Hayat <mubassirhayat@gmail.com>
2019-08-24 11:47:22 +0500

    APIController scaffoldings for Register, ClientSecret and Credetials resources

---
+++ app/Http/Controllers/APIs/ClientSecretAPIController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Meveto\Http\Controllers\APIs;
+
+use Illuminate\Http\Request;
+use Meveto\Http\Controllers\Controller;
+
+class ClientSecretAPIController extends Controller
+{
+    // 
+}
